cdb2d3f4901b9868cbb7c82f8fd15a30991fede9
zeonzeon-e <julia3277@naver.com>
2023-10-13 16:57:51 +0900

    0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './screen/Mainpage';
+import CommunityPage from './screen/CommunityPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/community' element={<CommunityPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
