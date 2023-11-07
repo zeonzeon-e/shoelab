986c6f2b536c37c7a0f0d680250e905957def596
zeonzeon-e <julia3277@naver.com>
2023-11-07 10:17:28 +0900

    spline export

--- src/screen/Custompage.js
+++ src/screen/Custompage.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "../css/reset.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Spline from '@splinetool/react-spline';
+import Space from '../components/Space';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import "../css/customspace.css";
 
 
 function CustomPage(){
     return(
         <div className="Page">
             <Header />
-            <div className="content">
-            <Spline scene="https://prod.spline.design/GuObGflEm9MJeK2h/scene.splinecode" />
+            <div className="threeSpace">
+            <Suspense fallback={null}>
+            <Canvas shadows flat linear>
+                <Space />
+                <OrbitControls />
+            </Canvas>
+            </Suspense>
             </div>
             <Footer />
         </div>
